f16811d107c8f1f2a6460b5f067f7e5edc30d64b
Kris <320499922+kristazim72-rgb@users.noreply.github.com>
2026-09-06 20:50:40 +0000

    Added variables for each part of the operation Created function operate to call math functions Created basic html calc with buttons for each digit, added operators and also clear and delete buttons

--- calc.js
+++ calc.js
@@ -1,16 +1,33 @@
 function add(a, b) {
     return a + b;
-}
+};
 
 function subtract(a, b) {
     return a - b;
-}
+};
 
 function multiply(a, b) {
     return a * b;
-}
+};
 
 function divide(a, b) {
-    if (b === 0) return "Error!";
+    if (b === 0) return "Snarky error!";
     return a / b;
+};
+
+let firstNum = "";
+let operator = null;
+let secondNumber = "";
+
+function operate(option, a, b){
+    a =Number(a);
+    b =Number(b);
+    switch (option) {
+        case "+": return add(a, b);
+        case "-": return subtract(a, b);
+        case "*": return multiply(a, b);
+        case "/": return divide(a, b);
+        default: return null;
+    }
 }
+
